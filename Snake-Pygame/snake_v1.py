dfc7f7a306774b62b26d1737bd946567624c54b0
Devonte Cunningham <devontedcr@yahoo.com>
2015-04-08 21:18:05 -0700

    Added losing features, restarts, movement

--- Snake-Pygame/snake_v1.py
+++ Snake-Pygame/snake_v1.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import random
 
 pygame.init()
 
@@ -8,47 +10,98 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
-gameDisplay = pygame.display.set_mode((800,600)) 
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height)) 
 pygame.display.set_caption('Snake 168') 
 
-gameExit = False;
 
-lead_x = 300
-lead_y = 300
-lead_x_change = 0
-lead_y_change = 0
 
 clock = pygame.time.Clock()
+block_size = 10
+FPS = 30
+
+font = pygame.font.SysFont(None,25)
 ###VARIABLES------------------------
+
+###FUNCTIONS------------------------
+def message_to_screen(msg,color):
+    screen_text = font.render(msg,True,color)
+    gameDisplay.blit(screen_text,[display_width/2,display_height/2])
+###FUNCTIONS------------------------
+
+
+
+
 
 
 
 ###GAME LOOP------------------------
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
+def gameLoop():
+    gameExit = False
+    gameOver = False
+    lead_x = display_width/2
+    lead_y = display_height/2
+    lead_x_change = 0
+    lead_y_change = 0
+    
+    while not gameExit:
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                lead_x_change =  -10
-            if event.key == pygame.K_RIGHT:
-                lead_x_change = 10
-          
-                
-    lead_x += lead_x_change
-   
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("Game over, press C to play again or Q to quit",red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+                        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change =  -block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_UP:
+                    lead_y_change =  -block_size
+                    lead_x_change = 0
+                elif event.key == pygame.K_DOWN:
+                    lead_y_change = block_size
+                    lead_x_change = 0
+                    
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+            gameOver = True
             
-    gameDisplay.fill(white)     
-    pygame.draw.rect(gameDisplay, black,[lead_x,lead_y,10,10])
+           
+              
+                    
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+       
+                
+        gameDisplay.fill(white)     
+        pygame.draw.rect(gameDisplay, black,[lead_x,lead_y,block_size,block_size])
+        
+        
+        pygame.display.update()
+        clock.tick(FPS)
     
-    
-    pygame.display.update()
-    clock.tick(15)
+    pygame.quit() 
+    quit()
 
 ###GAME LOOP-------------------------
         
+gameLoop()
+    
 
 
-pygame.quit() 
-quit() 
