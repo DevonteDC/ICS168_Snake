5300a5839b0ed8c20e9432d01b0a79bd7586ecd3
Devonte Cunningham <devontedcr@yahoo.com>
2015-04-13 17:22:28 -0700

    Centered text

--- Snake-Pygame/snake_v1.py
+++ Snake-Pygame/snake_v1.py
@@ -27,9 +27,14 @@ font = pygame.font.SysFont(None,25)
 ###VARIABLES------------------------
 
 ###FUNCTIONS------------------------
+def text_objects(text,color):
+    textSurface = font.render(text,True,color)
+    return textSurface, textSurface.get_rect()
 def message_to_screen(msg,color):
-    screen_text = font.render(msg,True,color)
-    gameDisplay.blit(screen_text,[display_width/2,display_height/2])
+    textSurf, textRect = text_objects(msg,color)
+    textRect.center = (display_width / 2), (display_height / 2)
+    gameDisplay.blit(textSurf,textRect)
+    
 
 def snake(block_size,snakeList):
     for XnY in snakeList:
@@ -125,12 +130,6 @@ def gameLoop():
         pygame.display.update()
 
 
-
-##        if lead_x >= randAppleX and lead_x <= randAppleX + AppleThickness:
-##            if lead_y >= randAppleY and lead_y <= randAppleY + AppleThickness:
-##                randAppleX = round(random.randrange(0,display_width - block_size)/10.0) * 10.0
-##                randAppleY = round(random.randrange(0,display_height - block_size)/10.0) * 10.0
-##                snakeLength += 1
 
         if lead_x > randAppleX and lead_x < randAppleX + AppleThickness or lead_x + block_size > randAppleX and lead_x + block_size < randAppleX + AppleThickness:
             if lead_y > randAppleY and lead_y < randAppleY + AppleThickness or lead_y + block_size > randAppleY and lead_y + block_size < randAppleY + AppleThickness:
