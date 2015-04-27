0bc964d20445fd25525dd21709ae15ce54f1d393
Devonte Cunningham <devontedcr@yahoo.com>
2015-04-27 01:27:05 -0700

    adding buttons

--- Snake-Pygame/snake_v1.py
+++ Snake-Pygame/snake_v1.py
@@ -8,8 +8,13 @@ pygame.init()
 
 white = (255,255,255)
 black = (0,0,0)
-red = (255,0,0)
+red = (200,0,0)
+light_red = (255,0,0)
 green = (0,155,0)
+light_green = (0,255,0)
+yellow = (200,200,0)
+light_yellow = (255,255,0)
+
 
 display_width = 800
 display_height = 600
@@ -100,10 +105,31 @@ def text_objects(text,color,size):
         textSurface = largefont.render(text,True,color)
     return textSurface, textSurface.get_rect()
 
+def text_to_button(msg, color, buttonx, buttony, buttonwidth, buttonheight,size = "small"):
+    textSurf, textRect = text_objects(msg,color,size)
+    textRect.center = (buttonx + (buttonwidth/2), buttony+(buttonheight/2))
+    gameDisplay.blit(textSurf,textRect)
+
 def message_to_screen(msg,color,y_displace = 0, size = 'small'):
     textSurf, textRect = text_objects(msg,color,size)
     textRect.center = (display_width / 2), (display_height / 2) + y_displace
     gameDisplay.blit(textSurf,textRect)
+
+def button(text,x,y,width,height,inactive_color,active_color,action = None):
+    cur = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    
+
+    if x + width > cur[0] > x and y + height > cur[1] > y:
+        pygame.draw.rect(gameDisplay,active_color,(x,y,width,height))
+        if click[0] == 1:
+            
+    else:
+        pygame.draw.rect(gameDisplay,inactive_color,(x,y,width,height))
+
+    text_to_button(text,black,x,y,width,height)
+        
+    
     
 
 def snake(block_size,snakeList):
@@ -157,9 +183,14 @@ def game_intro():
                           black,
                           50)
 
-        message_to_screen("Press C to play, P to pause, or Q to quit",
-                          black,
-                          180)
+        #message_to_screen("Press C to play, P to pause, or Q to quit",
+                          #black,
+                          #180)
+        
+
+        button("play",150,500,100,50,green,light_green, action = 'play')
+        button("controls",350,500,100,50,yellow,light_yellow,action='controls')
+        button("quit",550,500,100,50,red,light_red,action='quit')
 
         
 
